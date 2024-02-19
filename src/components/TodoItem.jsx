4e51bee44c51faf2fb20cb0736e28255345ae5c6
Adil Zhexenov <adilzhexenoff@gmail.com>
2024-02-19 21:09:38 +0600

    add classnames library

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import classnames from "classnames";
+
+// React Hook Form (Formik) - работа с формами
+// Axios - взаимодействия с сервером
+// ClassNames - работы с классами
 
 function TodoItem({ isCompleted, name, toggleTaskCompletion, deleteTask }) {
   return (
-    <li className={isCompleted ? "todo todo__completed" : "todo"}>
+    // использование classnames
+    <li className={classnames("todo", { todo__completed: isCompleted })}>
       <span className="todo-border"></span>
       <div className="todo-part">
         <div
